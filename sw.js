7bf9363a4e383ca48f03b5ea0627f4f45035eeb1
pedrocobron-ops <pedrocobron-ops@users.noreply.github.com>
2026-08-20 15:35:17 +0000

    Deploy do StageCue 99e913799d8a58b12ed8c1142778cd2680783d1d

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // GERADO por scripts/build.mjs
-const CACHE = 'stagecue-1wr82y5';
+const CACHE = 'stagecue-1evvjp7';
 self.addEventListener('install', e => { self.skipWaiting(); });
 self.addEventListener('activate', e => {
   e.waitUntil(caches.keys()
